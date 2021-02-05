ea7dda8e93744a8c8c77febc237d3ac61de6c8e3
Anna Jose <joann23july@gmail.com>
2021-02-05 01:02:23 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,76 @@
+const form = document.querySelector('form');
+
+form.addEventListener('submit', event =>{
+    event.preventDefault();
+    calculateSalaryHandler();
+})
+
+calculateSalary = (
+  employeeName,
+  grossPay,
+  incomeTax,
+  employeeInsurance,
+  cpp,
+  additions
+) => {
+  const totalDeductions =
+    parseInt((grossPay * incomeTax) / 100) +
+    parseInt((grossPay * employeeInsurance) / 100) +
+    parseInt((grossPay * cpp) / 100);
+  const finalSalary = grossPay - totalDeductions + additions;
+
+  const salaryObj = {
+    employeeName: employeeName,
+    grossPay: grossPay,
+    totalDeductions: totalDeductions,
+    additions: additions,
+    finalSalary: finalSalary,
+  };
+
+  generateSalarySlip(salaryObj);
+};
+
+calculateSalaryHandler = () => {
+  const employeeName = document.getElementById("emp-name").value;
+  let grossPay = parseInt(document.getElementById("gross-pay").value);
+  const bonus = document.getElementById("bonus").value;
+  const allowance = document.getElementById("allowance").value;
+  let incomeTax = document.getElementById("income-tax").value;
+  const employeeInsurance = document.getElementById("emp-insurance").value;
+  const cpp = document.getElementById("cpp").value;
+  const empGender = document.getElementById("male");
+  const gender = empGender.checked ? "male" : "female";
+  const dependencies = document.querySelector(".dependency-checkbox:checked")
+    .value;
+  const additions = parseInt(bonus) + parseInt(allowance);
+
+  if (gender === "female" && dependencies != 2) {
+    incomeTax = dependencies == 3 ? incomeTax - 4 : incomeTax - 6;
+  } else if (gender === "female" && dependencies == 2) {
+    incomeTax = incomeTax - 2;
+  } else if (gender === "male" && dependencies != 2) {
+    incomeTax = dependencies == 3 ? incomeTax - 2 : incomeTax - 4;
+  }
+
+  calculateSalary(
+    employeeName,
+    grossPay,
+    incomeTax,
+    employeeInsurance,
+    cpp,
+    additions
+  );
+};
+
+generateSalarySlip = (salaryObj) => {
+  document.getElementById("salary-calculator").style.display = "none";
+  document.getElementById("salary-slip").style.display = "block";
+
+  document.getElementById("employee-name").innerHTML = salaryObj.employeeName;
+  document.getElementById("gross-salary").innerHTML = salaryObj.grossPay;
+  document.getElementById("total-deduction").innerHTML =
+    salaryObj.totalDeductions;
+  document.getElementById("total-addition").innerHTML = salaryObj.additions;
+  document.getElementById("final-salary").innerHTML = salaryObj.finalSalary;
+};
+
